b0ebcaf8a07b3d4cb29af977b650b3ea943ef561
MadhushaPrasad <madushaprasad21@gmail.com>
2021-12-01 22:14:28 +0530

    feat(service):implemented authentocation service

---
+++ src/services/authentication.service.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const api_url = process.env.BASE_URL;
+
+class AuthenticationService {
+
+    login(user) {
+        return axios.post(api_url, user);
+    }
+
+    register(user) {
+        return axios.post(api_url, user);
+    }
+
+}
+
+export default new AuthenticationService();
