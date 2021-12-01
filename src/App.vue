c9cd72d4884d2b3f4ba541ad4fe8044384eb0e62
MadhushaPrasad <madushaprasad21@gmail.com>
2021-12-01 18:32:07 +0530

    feat(router):create router

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,41 @@
 <template>
-  <p>Hello World</p>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-dark">
+      <div class="container-fluid">
+        <a class="navbar-brand text-white" href="https://vuejs.org">
+          <img src="./assets/logo.png" alt="logo" height="40"/>
+          Vue
+        </a>
+        <button class="navbar-toggler bg-white" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon bg-white"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link text-white active" aria-current="page" href="/admin">Amin</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-white active" href="/">Home</a>
+            </li>
+          </ul>
+          <div class="d-flex flex-lg-row flex-sm-column">
+            <p class="nav-item">
+              <a class="ps-0 nav-link" href="/register">Sign Up</a>
+            </p>
+            <p class="nav-item">
+              <a class="ps-0 nav-link" href="/login">Sign In</a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </nav>
+
+    <div class="container">
+      <router-view></router-view>
+    </div>
+  </div>
 </template>
 
 <style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,62 @@
-import { createRouter, createWebHistory } from "vue-router";
+import {createRouter, createWebHistory} from "vue-router";
 import Home from "../views/Home.vue";
+import Profile from "@/views/Profile";
+import Admin from "@/views/Admin";
+import Register from "@/views/Register";
+import Login from "@/views/Login";
+import NotFound from "@/views/NotFound";
+import Unauthorized from "@/views/Unauthorized";
 
 const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home,
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
-  },
+    {
+        path: "/",
+        name: "home",
+        component: Home,
+    },
+    {
+        path: "/admin",
+        name: "admin",
+        component: Admin,
+    },
+    {
+        path: "/profile",
+        name: "profile",
+        component: Profile,
+    },
+    {
+        path: "/register",
+        name: "register",
+        component: Register,
+    },
+    {
+        path: "/login",
+        name: "login",
+        component: Login,
+    },
+    {
+        path: "/404",
+        name: "404",
+        component: NotFound,
+    },
+    {
+        path: "/401",
+        name: "401",
+        component: Unauthorized,
+    },
+    // {
+    //   path: "/about",
+    //   name: "About",
+    //   // route level code-splitting
+    //   // this generates a separate chunk (about.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () =>
+    //     import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    // },
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes,
+    history: createWebHistory(process.env.BASE_URL),
+    routes,
 });
 
 export default router;
