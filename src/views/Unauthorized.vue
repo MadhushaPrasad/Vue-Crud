ec8c0d640cee13aa3ce08c9f40c4301e4b88a272
MadhushaPrasad <madushaprasad21@gmail.com>
2021-12-01 21:36:53 +0530

    feat(404,401):implemented pages

--- src/views/Unauthorized.vue
+++ src/views/Unauthorized.vue
@@ -1,5 +1,17 @@
 <template>
-  <div>Unauthorized</div>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12 text-center">
+        <span class="display-1">
+          401
+        </span>
+        <div class="mb-4 lead">
+          Unauthorized! Access to this resource is denied.
+        </div>
+        <router-link to="/" class="btn btn-link">Back to Home</router-link>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
