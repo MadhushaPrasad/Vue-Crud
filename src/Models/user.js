75c144b1c7567f2b250003c230290c6c4e6353f9
MadhushaPrasad <madushaprasad21@gmail.com>
2021-12-01 21:43:40 +0530

    feat(user model):implemented user model

---
+++ src/Models/user.js
@@ -0,0 +1,9 @@
+export default class User {
+    constructor(id, userName, password, name, role, token) {
+        this.id = id;
+        this.userName = userName;
+        this.password = password;
+        this.name = role;
+        this.token = token;
+    }
+}
